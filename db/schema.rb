3c0e6360c6105f40b1c2e08b15f3381f6ab47f4b
MarenWoodruff <wren403@gmail.com>
2015-10-29 12:37:36 -0400

    added notes to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029153733) do
+ActiveRecord::Schema.define(version: 20151029163603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151029153733) do
     t.string   "state"
     t.string   "zip_code"
     t.string   "phone_number"
+    t.text     "notes"
   end
 
   create_table "hotels", force: :cascade do |t|
